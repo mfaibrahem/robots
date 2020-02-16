8282e8e69a3af2ff97c793044d2ca75feceb5abb
mfaibrahem <mfaibrahem2013@gmail.com>
2020-02-16 17:40:23 +0200

    Updates

---
+++ precache-manifest.1a1daed21b703711aea3561dbdbf47d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c93399685931ae77e6196c28bced5b",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "c3e697c22a23542ac4f6",
+    "url": "/robots/static/css/main.c8ce1393.chunk.css"
+  },
+  {
+    "revision": "2618239111a9eb159587",
+    "url": "/robots/static/js/2.325f9c96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots/static/js/2.325f9c96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3e697c22a23542ac4f6",
+    "url": "/robots/static/js/main.26a2ed21.chunk.js"
+  },
+  {
+    "revision": "33872d91c43410f50ba7",
+    "url": "/robots/static/js/runtime-main.962b2a3d.js"
+  }
+]);
